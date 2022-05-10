71a20dc545bdcf5d21894988e8e0fa0c326e879c
zulvanF <zulvanfahmi1612@gmail.com>
2022-05-10 12:06:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container : {
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        primary : '#112D4E',
+        secondary : '#3F72AF',
+        bg2 : '#DBE2EF',
+        bg1 : '#F9F7F7',
+      },
+      screens : {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
